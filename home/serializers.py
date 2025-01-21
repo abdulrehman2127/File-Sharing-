4c87882b27ae179016308235ddc2e2e478a148e2
abdulrehman <abdulrehmanabid2127@gmail.com>
2025-01-21 09:03:38 +0000

    First Commit

---
+++ home/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Files,Folder 
+import shutil
+from numpy import require
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Files
+        fields='__all__'
+
+
+class FileListSerializer(serializers.Serializer):
+    files = serializers.ListField(
+        child = serializers.FileField(max_length=100000 , allow_empty_file=False , use_url=False)
+    )
+    folder=serializers.CharField(required=False)
+
+    def zip_files(self,folder):
+        shutil.make_archive(f'public/static/zip/{folder}' , 'zip' , f'public/static/{folder}')
+
+
+
+    def create(self,validated_data):
+        folder = Folder.objects.create()
+        files = validated_data.pop('files')
+        file_objects=[]
+        for file in files:
+            files_obj=Files.objects.create(folder = folder , file = file)
+            file_objects.append(files_obj)
+
+
+        self.zip_files(folder.uid)
+
+        return {'files':{},
+                 'folder':str(folder.uid)}
+    
+
